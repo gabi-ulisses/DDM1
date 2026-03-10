97021126286b8eef42df2f09695d0f5e71d4c539
Gabrielle Ulisses <82672178+gabi-ulisses@users.noreply.github.com>
2026-03-10 20:22:56 -0300

    added: Add multiplication table calculation

---
+++ exercises/src/introduction/ex6.kt
@@ -0,0 +1,15 @@
+fun calculationTabuada(n: Int){
+    for(i in 0 .. 10){
+        val res = n * i;
+        println("$n * $i = $res")
+    }
+}
+
+fun main() {
+
+    println("Vamos calcular a tabuada de um número!")
+    print("Número: ")
+    val num  = readln().toInt()
+
+    println("Tabuada do $num:")
+    calculationTabuada(num)
