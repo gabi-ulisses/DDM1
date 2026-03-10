51f1f7b4db048f7598759954554aaae50137f143
Gabrielle Ulisses <82672178+gabi-ulisses@users.noreply.github.com>
2026-03-10 20:20:58 -0300

    added: program to calculate circle area

---
+++ exercises/src/introduction/ex4.kt
@@ -0,0 +1,18 @@
+onst val pi: Double = 3.14159
+
+fun calculationArea(r: Double): Double{
+    val a = pi * (r*r)
+    return  a
+}
+
+fun main() {
+
+    println("Vamos calcular a área de um círculo!")
+    print("Raio: ")
+    val raio  = readln().toDouble()
+
+    val area = calculationArea(raio)
+
+    println("O círculo com raio $raio tem uma área de $area metros quadrados.")
+
+}
