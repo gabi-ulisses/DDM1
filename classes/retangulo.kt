7158968a78ee85e9c40c3d28bc67bca75dafc146
Gabrielle Ulisses <82672178+gabi-ulisses@users.noreply.github.com>
2026-03-10 21:55:47 -0300

    added: Add area property to Retangulo class
    Added area property to calculate rectangle area.

--- classes/retangulo.kt
+++ classes/retangulo.kt
@@ -2,19 +2,26 @@ package classes
 
 class Retangulo (
     val largura: Int,
-    val altura: Int
+    val altura: Int,
 ) {
     val isSquare: Boolean
         get(){                                  // info: getter personalizado
             return largura == altura            // info: syntax feia
         }
-}
+
+    val area: Int
+        get(){
+            return largura * altura
+        }
+    }
 
 fun main() {
     val r1 = Retangulo(10,20)
     val r2 = Retangulo(10,10)
 
     println("r1: " + r1.isSquare)
+    println("area r1: " + r1.area)
     println("r2: " + r2.isSquare)
+    println("area r2: " + r2.area)
 
 }
